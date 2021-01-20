24bda4d8e6121b77deb29583bc6ecfd389fa20a7
szerg97 <szerg97@gmail.com>
2021-01-20 20:16:58 +0100

    Revert "appservice commit"
    This reverts commit d5ef5933126671a9a5d7b09f02513308115ec3ed.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -17,6 +17,7 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddScoped<ICompanyRepository, CompanyRepository>();
